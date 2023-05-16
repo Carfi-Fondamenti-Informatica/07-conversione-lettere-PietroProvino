3dcfb3c4795cfae4a61e67c9ad11a6f6bbc8209a
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-05-16 09:53:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     cin >> b;
     a=b;
     if(verifica_lettera(a) == true ){
-        cout << "Vero";
+        cout << "Vero" << endl;
         cout << Trasforma_Maiusc(a);
     }
     else{
