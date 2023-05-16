d7f3a6817cb126f247fb414855e10154a0571e2a
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-05-16 09:53:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char b;
+    int a;
+    cin >> b;
+    a=b;
+    if(verifica_lettera(a) == true ){
+        cout << "Vero";
+        cout << Trasforma_Maiusc(a);
+    }
+    else{
+        cout << "errore";
+    }
 }
