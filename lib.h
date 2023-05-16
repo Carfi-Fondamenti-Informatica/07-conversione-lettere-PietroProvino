55d8e0efbde82d1feca006ff60ce3008bcee5f01
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-05-16 09:52:33 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,12 @@
+//
+// Created by pietr on 16/05/2023.
+//
 
+#ifndef ESERCITAZIONE7_LIB_H
+#define ESERCITAZIONE7_LIB_H
+
+#endif //ESERCITAZIONE7_LIB_H
+
+bool verifica_lettera(int a);
+
+char Trasforma_Maiusc(int a);
