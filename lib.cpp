1864d42dec0b86e7e6e62bfcf4b3217883c7ade8
PietroProvino <127682352+PietroProvino@users.noreply.github.com>
2023-05-16 13:39:07 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,20 +1,12 @@
 #include "lib.h"
 
- bool verifica_lettera(int a){
-    if (((a>=65) && (a<=90)) or ((a>=97) &&( a<=122))){
+bool caratteri(char &x){
+    if(x >= 65 && x <= 90){
+        x += 32;
         return true;
-    }
-    else{
+    }else if(x >= 97 && x <= 122){
+        x -= 32;
+        return true;
+    }else
         return false;
-    }
-}
-
-char Trasforma_Maiusc(int a){
-    if ((a>=97) &&( a<=122)) {
-        a=a-32;
-        return a;
-    }
-    else{
-        return a;
-    }
 }
